04efe95900f220d8c4e8d86d2cb5443acb1c9ac8
noubuddy <anton.nich00@gmail.com>
2021-11-18 11:34:04 +0200

    Lcd test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,26 @@
 #include <Arduino.h>
 #include <Wire.h>
-
-#include <LiquidCrystal_I2C.h> // lcd 16 02
-#include <Keypad.h> // membrannaja klava
-
-// maxon
+#include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
-// pult
 #include <IRremote.h>
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
-  
+  // init lcd
+  lcd.begin(16, 2);
+  // turn on the backlight
+  lcd.backlight();
 }
 
 void loop() {
-  
+  // clear lcd
+  lcd.clear();
+  // set cursor to the begining of the lcd
+  lcd.setCursor(0, 1);
+  // print message
+  lcd.print("Hello");
 }
