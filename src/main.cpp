9dc38efb3ef24df790682eb564ac7073bd0ca27b
Anton Nitiportik <TA-19V-299@tptlive.ee>
2021-11-18 11:11:30 +0200

    Remove useless code

--- src/main.cpp
+++ src/main.cpp
@@ -8,48 +8,13 @@
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
-
 // pult
 #include <IRremote.h>
 
-#define RECV_PIN 47
-
-IRrecv irrecv(RECV_PIN);
-decode_results results;
-
-const byte ROWS = 4; //four rows
-const byte COLS = 4; //four columns
-char keys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
-byte rowPins[ROWS] = {23, 25, 27, 29}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {31, 33, 35, 37}; //connect to the column pinouts of the keypad
-
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
-
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-
 void setup() {
-  // put your setup code here, to run once:
-  lcd.init();
-	lcd.backlight();
-  Serial.begin(9600);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (Serial.available()) {
-    // wait a bit for the entire message to arrive
-    delay(100);
-    // clear the screen
-    lcd.clear();
-    // read all the available characters
-    while (Serial.available() > 0) {
-      // display each character to the LCD
-      lcd.write(Serial.read());
-    }
-  }
+  
 }
